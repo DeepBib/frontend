3f343f92946d48df6cb6717354de593afedde090
José Daniel Organista <josedaniel@agrapp.co>
2022-03-22 15:28:58 +0100

    test connection with back

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-// import SearchBar from "./SearchBar";
-// import CheckBoxAPI from "./CheckBoxAPI";
+import axios from 'axios';
 
 // const API = [
 //     'i3e',
@@ -12,52 +11,33 @@ import React, { useState } from 'react';
 
 const SearchForm = (props) => {
 
-    const [query, SetQuery] = useState(props.query || '');
+    const [query, setQuery] = useState(props.query || '');
+    const [response, setResponse] = useState("");
 
-    // function toggleCheckbox (label){
-    //     if (this.selectedCheckboxes.has(label)) {
-    //       this.selectedCheckboxes.delete(label);
-    //     } else {
-    //       this.selectedCheckboxes.add(label);
-    //     }  
-    // }
-
-    // handleSubmit(event) {
-    //     alert('A search was submitted: ' + this.state.query);
-    //     event.preventDefault();
-    // }
-     
-    // createAPIsection(){
-    //     API.map(this.createCheckBox)
-    // }
-
-    // function createCheckBox(label){
-    //     return (<CheckBoxAPI
-    //         label={label}
-    //         handleCheckboxChange={this.toggleCheckbox}
-    //     />);
-    // }
-
-    // const createAPIsection = () => {
-    //     API.map(this.createCheckBox)
-    // }
+    const fetchData = (api,query) => {
+        axios.get(`http://localhost:8000/articles/${query}`)
+            .then(response => {
+                console.log(response);
+                setResponse(response);
+            }).catch(error => {
+                console.log(error);
+            })
+    }
 
     return(
         <div>
             <div className="search-row row justify-content-center">
                 <div className="form-group col-6">
-                    <input type="text" value={query} onChange={(e) => SetQuery(e.target.value)} name="query" className="form-control" placeholder="Search" required autocomplete="off"></input>
+                    <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} name="query" className="form-control" placeholder="Search" required autocomplete="off"></input>
                 </div>
 
                 <div className="col-1">
-                    <button className="btn" onClick={() => console.log(query)}>Search</button>
+                    <button className="btn" onClick={() => fetchData(query)}>Search</button>
                 </div>
                     
             </div>
             <div className="API-row form-inline">
                 <div className="API-form-text form-check-inline">Bib :</div>
-                
-                {/* {this.createAPIsection()}                    */}
             </div>
         </div> 
     );
